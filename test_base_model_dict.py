0bfd804b81c2bfb4797468ca6d1d50fb52c7af7c
simplykevine <umutonikevine2004>
2023-12-11 02:26:11 -0800

    kevine

--- test_base_model_dict.py
+++ test_base_model_dict.py
@@ -12,13 +12,13 @@ my_model_json = my_model.to_dict()
 print(my_model_json)
 print("JSON of my_model:")
 for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+    value_type = type(my_model_json[key])
+    value = my_model_json[key]
+    print("\t{}: ({}) - {}".format(key, value_type, value))
 print("--")
 my_new_model = BaseModel(**my_model_json)
 print(my_new_model.id)
 print(my_new_model)
 print(type(my_new_model.created_at))
-
 print("--")
 print(my_model is my_new_model)
-
